1eca6885899cc6189bc832dc72af6778ce639b7c
zxx <19834425346@163.com>
2024-01-25 00:20:56 +0800

    init

---
+++ assets/script/loginSenceMgr.ts
@@ -0,0 +1,16 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('login')
+export class login extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+
+    public onLoginBtnClick() { }
+}
+
